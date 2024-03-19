aa262421e50b495add4876fd7cf6faf0c0bafbb9
Eduardo-Vinicius <53955680+Eduardo-Vinicius@users.noreply.github.com>
2024-03-19 01:46:35 -0300

    configuracoes de rabbitmq event

--- src/infrastructure/repository/helper/connection.module.ts
+++ src/infrastructure/repository/helper/connection.module.ts
@@ -5,14 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
     imports: [
         TypeOrmModule.forRoot({
-            // url: 'mongodb://root:example@mongo:27017/fiap?authSource=admin',
+            url: 'mongodb://127.0.0.1:27017/fiap',
             type: 'mongodb',
-            host: process.env.DATABASE_HOST,
-            username: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_SCHEMA,
-            authSource: process.env.DATABASE_AUTHSOURCE,
-            port: Number(process.env.DATABASE_PORT),
+            //host: process.env.DATABASE_HOST,
+            //username: process.env.DATABASE_USERNAME,
+            //password: process.env.DATABASE_PASSWORD,
+            //database: process.env.DATABASE_SCHEMA,
+            //authSource: process.env.DATABASE_AUTHSOURCE,
+            //port: Number(process.env.DATABASE_PORT),
             entities: [PagamentoEntity],
             synchronize: true
         })
